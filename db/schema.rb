452df128b7eedf6a7590b97fe5760f11424a37a2
Nino Bozzi <n.bozzi@mbmlatam.cl>
2020-05-18 17:36:27 -0400

    After making first run. Changed ruby version in Gemfile to 2.6.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_200411) do
+ActiveRecord::Schema.define(version: 2019_07_29_210825) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
