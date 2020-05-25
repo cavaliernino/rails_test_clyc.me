7f5c824e57ac1661a2df7c0709f456895b463891
Nino Bozzi <n.bozzi@mbmlatam.cl>
2020-05-25 12:25:30 -0400

    First Dijkstra failed test fixed. Dijkstra algorithm implemented.

--- lib/data_analytics.rb
+++ lib/data_analytics.rb
@@ -1,10 +1,11 @@
 class Graph
-  attr_reader :graph, :nodes, :previous, :distance 
+  attr_reader :graph, :nodes, :previous, :distance
   INFINITY = 1 << 64
 
   def initialize
     @graph = {}
-    @nodes = Array.new
+    @prev = {}
+    @nodes = []
   end
 
   def connect_graph(source, target, weight)
@@ -23,9 +24,48 @@ class Graph
     connect_graph(target, source, weight)
   end
 
+  def min_distance_node(nodes)
+    min_distance = INFINITY
+    min_distance_node = nil
+    nodes.each do |node|
+      if @distance[node] < min_distance
+        min_distance_node = node
+      end
+    end
+    min_distance_node
+  end
+
+  def neighbors(node, node_list)
+    neighbor_dict = @graph[node]
+    neighbor_dict.each do |neighbor|
+      unless node_list.include? neighbor
+        neighbor_dict.delete neighbor
+      end
+    end
+    neighbor_dict
+  end
 
   def dijkstra(source)
+    @distance = {}
+    unvisited = []
+    @nodes.each do |node|
+      @distance.store(node, INFINITY)
+      @prev.store(node, nil)
+      unvisited << node
+    end
+    @distance.store(source, 0)
 
+    while unvisited.any?
+      node = min_distance_node(unvisited)
+      unvisited.delete(node)
+      neighbors(node, unvisited).each do |neighbor|
+        alt = @distance[node] + @graph[node][neighbor[0]]
+        if alt < @distance[neighbor[0]]
+          @distance[neighbor[0]] = alt
+          @prev[neighbor[0]] = node
+        end
+      end
+    end
   end
 
   def find_path(dest)
